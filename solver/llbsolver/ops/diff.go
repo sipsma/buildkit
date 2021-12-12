1a054d6fd8b5500376cbad0f323b5843269eca18
Erik Sipsma <erik@sipsma.dev>
2021-12-12 12:52:12 -0800

    wip solver llb unmerge
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- solver/llbsolver/ops/diff.go
+++ solver/llbsolver/ops/diff.go
@@ -21,6 +21,7 @@ const diffCacheType = "buildkit.diff.v0"
 type diffOp struct {
 	op     *pb.DiffOp
 	worker worker.Worker
+	vtx    solver.Vertex
 }
 
 func NewDiffOp(v solver.Vertex, op *pb.Op_Diff, w worker.Worker) (solver.Op, error) {
@@ -30,6 +31,7 @@ func NewDiffOp(v solver.Vertex, op *pb.Op_Diff, w worker.Worker) (solver.Op, err
 	return &diffOp{
 		op:     op.Diff,
 		worker: w,
+		vtx:    v,
 	}, nil
 }
 
@@ -66,6 +68,66 @@ func (d *diffOp) CacheMap(ctx context.Context, group session.Group, index int) (
 }
 
 func (d *diffOp) Exec(ctx context.Context, g session.Group, inputs []solver.Result) ([]solver.Result, error) {
+	// TODO: handle when lower and/or upper are empty
+	lowerEdge := d.vtx.Inputs()[d.op.Lower.Input]
+	upperEdge := d.vtx.Inputs()[d.op.Upper.Input]
+	var unmergeEdges []solver.Edge
+	var isUnmerge bool
+findUnmergePath:
+	for {
+		if upperEdge.Vertex.Digest() == lowerEdge.Vertex.Digest() && upperEdge.Index == lowerEdge.Index {
+			isUnmerge = true
+			break
+		}
+		unmergeEdges = append(unmergeEdges, upperEdge)
+		if baseOp, ok := upperEdge.Vertex.Sys().(*pb.Op); ok {
+			switch op := baseOp.Op.(type) {
+			case *pb.Op_Exec:
+				var mnt *pb.Mount
+				for _, m := range op.Exec.Mounts {
+					// TODO: not sure if this is correct
+					if int(m.Output) == int(upperEdge.Index) {
+						mnt = m
+						break
+					}
+				}
+				if mnt == nil {
+					break findUnmergePath
+				}
+				upperEdge = upperEdge.Vertex.Inputs()[mnt.Input]
+			case *pb.Op_File:
+				var action *pb.FileAction
+				for _, a := range op.File.Actions {
+					if int(a.Output) == int(upperEdge.Index) {
+						action = a
+						break
+					}
+				}
+				if action == nil {
+					break findUnmergePath
+				}
+				upperEdge = upperEdge.Vertex.Inputs()[action.Input] // TODO: right? not secondary input?
+			case *pb.Op_Merge:
+			case *pb.Op_Source, *pb.Op_Merge, *pb.Op_Diff, *pb.Op_Build:
+				// these ops are all bases in that they can't be layered on top of existing layers
+				break findUnmergePath
+			default:
+				// TODO: should never be here, log it?
+				break findUnmergePath
+			}
+		}
+	}
+
+	if isUnmerge {
+		// create the result as a merging of diffs separating lower and upper
+	}
+
+	//
+	//
+	//
+	//
+	//
+
 	var curInput int
 
 	var lowerRef cache.ImmutableRef
