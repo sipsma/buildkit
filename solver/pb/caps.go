f69c0798296a13cfe253ac100c83f06eef838388
Tõnis Tiigi <tonistiigi@gmail.com>
2024-10-08 08:28:40 -0700

    Merge pull request #5359 from tonistiigi/gc-free-max-support
    update default and basic gc control to use free and max storage
    
    (cherry picked from commit 6860c8097dec4cdfdf2ca759214b7dd3bf5c7c61)

--- solver/pb/caps.go
+++ solver/pb/caps.go
@@ -91,6 +91,9 @@ const (
 	CapMultipleExporters apicaps.CapID = "exporter.multiple"
 
 	CapSourcePolicy apicaps.CapID = "source.policy"
+
+	// GC/Prune controls allow MinFreeSpace and MaxUsedSpace to be set
+	CapGCFreeSpaceFilter apicaps.CapID = "gc.freespacefilter"
 )
 
 func init() {
@@ -485,6 +488,12 @@ func init() {
 
 	Caps.Init(apicaps.Cap{
 		ID:      CapSourcePolicy,
+		Enabled: true,
+		Status:  apicaps.CapStatusExperimental,
+	})
+
+	Caps.Init(apicaps.Cap{
+		ID:      CapGCFreeSpaceFilter,
 		Enabled: true,
 		Status:  apicaps.CapStatusExperimental,
 	})
